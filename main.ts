5b36435bb1f29aa6e0f871e9d86c3ac5d151a889
Dzaki Zuhditiya <110822574+NewsBee@users.noreply.github.com>
2023-06-23 13:26:51 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
+namespace Edumath {
+    // Blok untuk memeriksa apakah suatu bilangan genap
+    //% block="Apakah $n bilangan ganjil atau genap"
+    export function isEvenNumber(n: number): void {
+    if (n % 2 === 0) {
+        basic.showString("Even");
+    } else {
+        basic.showString("Odd");
+     }
+    }
+
+
+    // Blok untuk mencari faktorial suatu bilangan
+    //% block="Faktorial dari $n"
+    export function factorial(n: number): number {
+        let result = 1;
+        for (let i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // Blok untuk menjumlahkan dua bilangan
+    // dan mengembalikan hasil penjumlahannya
+    //% block="Penjumlahan dari $a + $b"
+    export function addNumbers(a: number, b: number): number {
+        return a + b;
+    }
+}
